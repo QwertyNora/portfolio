2f185ee66b9c0b3409c16297c6fe165dbb5bda1b
Nora Silfver <nora.silfver@gmail.com>
2026-02-25 14:36:02 +0100

    Add IProjectRepository interface for project management operations

---
+++ src/Portfolio.Application/Interfaces/IProjectRepository.cs
@@ -0,0 +1,12 @@
+using Portfolio.Domain.Entities;
+
+namespace Portfolio.Application.Interfaces;
+
+public interface IProjectRepository
+{
+    Task<IEnumerable<Project>> GetAllAsync();
+    Task<Project?> GetByIdAsync(int id);
+    Task<Project> CreateAsync(Project project);
+    Task<Project> UpdateAsync(Project project);
+    Task DeleteAsync(int id);
+}
