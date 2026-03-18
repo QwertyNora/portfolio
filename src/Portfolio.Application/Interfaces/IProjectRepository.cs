12fc30f307794d872d30499ab3e1e6900927f32e
Nora Silfver <nora.silfver@gmail.com>
2026-03-18 17:14:03 +0100

    Remove CRUD methods from IProjectRepository interface

--- src/Portfolio.Application/Interfaces/IProjectRepository.cs
+++ src/Portfolio.Application/Interfaces/IProjectRepository.cs
@@ -6,7 +6,4 @@ public interface IProjectRepository
 {
     Task<IEnumerable<Project>> GetAllAsync();
     Task<Project?> GetByIdAsync(int id);
-    Task<Project> CreateAsync(Project project);
-    Task<Project> UpdateAsync(Project project);
-    Task DeleteAsync(int id);
 }
