a2da907f0f2167c34da4e6548102d8e8272694b3
Nora Silfver <nora.silfver@gmail.com>
2026-03-18 16:45:10 +0100

    Add StaticProjectRepository for managing project data

---
+++ src/Portfolio.Infrastructure/Repositories/Static/StaticProjectRepository.cs
@@ -0,0 +1,58 @@
+using Portfolio.Application.Interfaces;
+using Portfolio.Domain.Entities;
+
+namespace Portfolio.Infrastructure.Repositories.Static;
+
+public class StaticProjectRepository : IProjectRepository
+{
+    private static readonly List<Project> _projects =
+    [
+        new Project
+        {
+            Id = 1,
+            Title = "Portfolio",
+            Description = "Min personliga portfolio byggd med .NET och React.",
+            Technologies = ["C#", ".NET", "React", "TypeScript", "PostgreSQL"],
+            GitHubUrl = "https://github.com/QwertyNora/portfolio",
+            IsFeatured = true,
+            CreatedAt = new DateTime(2026, 1, 1)
+        },
+        new Project
+        {
+            Id = 2,
+            Title = "Platemates",
+            Description = "Beskrivning av projektet.",
+            Technologies = ["React", "Node.js"],
+            GitHubUrl = "https://github.com/QwertyNora/platemates",
+            IsFeatured = true,
+            CreatedAt = new DateTime(2025, 6, 1)
+        }
+    ];
+
+    public Task<IEnumerable<Project>> GetAllAsync() =>
+        Task.FromResult<IEnumerable<Project>>(_projects);
+
+    public Task<Project?> GetByIdAsync(int id) =>
+        Task.FromResult(_projects.FirstOrDefault(p => p.Id == id));
+
+    public Task<Project> CreateAsync(Project project)
+    {
+        project.Id = _projects.Max(p => p.Id) + 1;
+        _projects.Add(project);
+        return Task.FromResult(project);
+    }
+
+    public Task<Project> UpdateAsync(Project project)
+    {
+        var index = _projects.FindIndex(p => p.Id == project.Id);
+        if (index != -1) _projects[index] = project;
+        return Task.FromResult(project);
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        var project = _projects.FirstOrDefault(p => p.Id == id);
+        if (project is not null) _projects.Remove(project);
+        return Task.CompletedTask;
+    }
+}
