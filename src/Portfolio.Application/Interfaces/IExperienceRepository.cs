f9b503b344d5c8dfc9875aaa6969f91e1a5ba38e
Nora Silfver <nora.silfver@gmail.com>
2026-03-18 17:13:58 +0100

    Remove CRUD methods from IExperienceRepository interface

--- src/Portfolio.Application/Interfaces/IExperienceRepository.cs
+++ src/Portfolio.Application/Interfaces/IExperienceRepository.cs
@@ -6,7 +6,4 @@ public interface IExperienceRepository
 {
     Task<IEnumerable<Experience>> GetAllAsync();
     Task<Experience?> GetByIdAsync(int id);
-    Task<Experience> CreateAsync(Experience experience);
-    Task<Experience> UpdateAsync(Experience experience);
-    Task DeleteAsync(int id);
 }
