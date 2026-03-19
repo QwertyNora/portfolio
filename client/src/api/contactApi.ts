b7bb467aa6e2b59a2a1c6dbea572a17fcb075616
Nora Silfver <nora.silfver@gmail.com>
2026-03-19 14:54:02 +0100

    Update contactApi to use AdminContactMessage type in getContactMessages

--- client/src/api/contactApi.ts
+++ client/src/api/contactApi.ts
@@ -1,11 +1,11 @@
 import axiosInstance from "./axiosInstance";
-import type { ContactMessage } from "../types/contact";
+import type { ContactMessage, AdminContactMessage } from "../types/contact";
 
 export const sendContactMessage = async (message: ContactMessage): Promise<void> => {
     await axiosInstance.post("/contact", message);
 };
 
-export const getContactMessages = async (): Promise<ContactMessage[]> => {
+export const getContactMessages = async (): Promise<AdminContactMessage[]> => {
     const response = await axiosInstance.get("/contact");
     return response.data;
 };
